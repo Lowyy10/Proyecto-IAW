495ecadc3efd2e0419ec6311cd463e11542496b9
Lowyy10 <luismanuel.vadillo.vargas.alu@iesfernandoaguilar.es>
2024-10-08 19:56:48 +0200

    Reini

--- restaurante_reservas/reservas/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from .views import ReservaCreateView
-
-app_name = 'reservas'
-
-urlpatterns = [
-    path('nueva/', ReservaCreateView.as_view(), name='crear_reserva'),  # Ruta para crear una nueva reserva
-]
